40ffc671e1b7cf31995039841bfc95cf1c8bcfb0
Cristian Guerrero <cristian.guerrero-b@mail.escuelaing.edu.co>
2026-02-24 19:50:35 -0500

    Implementacion ChainResponsability

---
+++ src/main/java/dosw/bitacora/Semana3/patrones/ChainOfResponsibility/MotivoViajeControl.java
@@ -0,0 +1,16 @@
+package dosw.bitacora.Semana3.patrones.ChainOfResponsibility;
+
+public class MotivoViajeControl extends ControlMigratorioHandler {
+
+    @Override
+    public void procesar(IngresoRequest request) {
+        System.out.println("Evaluando motivo del viaje...");
+
+        if (!request.isMotivoValido()) {
+            System.out.println("Ingreso rechazado: Motivo de viaje no válido.");
+            return;
+        }
+
+        procesarSiguiente(request);
+    }
+}
